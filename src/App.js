48dc3324b8fd04251b97a90f06f77e6165df32b8
Tiancheng Wang <107882951+tiancheng710@users.noreply.github.com>
2022-06-24 18:33:13 -0400

    Upload Changes to App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TeachersList from './components/TeachersList';
 import StudentsList from './components/StudentsList'
 import MarkingsLists from './components/MarkingsLists';
-import ButtonMenu from './components/ButtonMenu';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 
-
-
-
-function App({ message }) {
+function App({ message })  {
   return (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <h1>{message}</h1>
-        <ButtonMenu />
-        <TeachersList/>
-        <StudentsList/>
-        <MarkingsLists/>
-      </header>
+      <ul className="App-header">
+      <h1>{message}</h1>
+      <li>
+        <Link to="/TeachersList">Teachers' List</Link>
+      </li>
+      <li>
+        <Link to="/StudentsList">Students' List</Link>
+      </li>
+      <li>
+        <Link to="/MarkingsLists">Markings</Link>
+      </li>
+      <Routes>
+        <Route exact path='/TeachersList' element={< TeachersList />}></Route>
+        <Route exact path='/StudentsList' element={< StudentsList />}></Route>
+        <Route exact path='/MarkingsLists' element={< MarkingsLists />}></Route>
+      </Routes>
+     </ul> 
     </div>
+  </Router>
+
   );
 }
 
